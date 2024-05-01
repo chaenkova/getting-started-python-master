08085c992f7b2e51db1848088e79b48f5516e499
Daria Chaenkova <dash.kustowa2010@yandex.ru>
2024-05-01 17:48:19 +0300

    first

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -5,7 +5,7 @@ def test_google_find_selene():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_do_not_find_coffe():
+def test_google_do_not_find():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.no.text('Coffe'))
